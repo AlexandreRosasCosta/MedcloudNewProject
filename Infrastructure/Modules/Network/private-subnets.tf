56cbd2bdf0e8423aabc3527542ef0c517e4910c4
= <=>
2023-07-24 12:51:10 -0300

    Feat: Added a Network module

--- Infrastructure/Modules/Network/private-subnets.tf
+++ Infrastructure/Modules/Network/private-subnets.tf
@@ -2,8 +2,9 @@ resource "aws_subnet" "priv-subnet-1a" {
   vpc_id     = aws_vpc.main.id
   cidr_block = cidrsubnet("${var.cidr_block}", 8, 3)
   availability_zone = "us-east-1a"
+  
   tags = {
-    Name = "Main"
+    Name = "private-subnet-1a"
   }
 }
 
@@ -11,7 +12,8 @@ resource "aws_subnet" "priv-subnet-1b" {
   vpc_id     = aws_vpc.main.id
   cidr_block = cidrsubnet("${var.cidr_block}", 8, 4)
   availability_zone = "us-east-1b"
+
   tags = {
-    Name = "Main"
+    Name = "private-subnet-1b"
   }
 }
--- Infrastructure/Modules/Network/public-subnets.tf
+++ Infrastructure/Modules/Network/public-subnets.tf
@@ -2,9 +2,10 @@ resource "aws_subnet" "pub-subnet-1a" {
   vpc_id     = aws_vpc.main.id
   cidr_block = cidrsubnet("${var.cidr_block}", 8, 1)
   availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
 
   tags = {
-    Name = "Main"
+    Name = "public-subnet-1a"
   }
 }
 
@@ -12,8 +13,9 @@ resource "aws_subnet" "pub-subnet-1b" {
   vpc_id     = aws_vpc.main.id
   cidr_block = cidrsubnet("${var.cidr_block}", 8, 2)
   availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
 
   tags = {
-    Name = "Main"
+    Name = "public-subnet-1b"
   }
 }
