24ef945678c9c6c53ee291f55152a860ddc6e8a1
Alexandre Rosas Costa <alexandrerosascosta@gmail.com>
2023-07-25 18:33:13 -0300

    Feat: Added path for ECS

--- Infrastructure/variables.tf
+++ Infrastructure/variables.tf
@@ -2,9 +2,3 @@ variable "cidr_block" {
   type        = string
   description = "IP for VPC and subnets"
 }
-
-variable "region" {
-  type        = string
-  description = "Region for project"
-  default     = "us-east-1"
-}
