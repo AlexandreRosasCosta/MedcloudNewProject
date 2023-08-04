dec1bda5d49a4c95ca4b00c377a4d141cc88355a
= <=>
2023-08-04 17:40:52 -0300

    Feat: Added alarm for the ecs with cloudwatch

--- Infrastructure/Modules/Services/CloudWatch/main.tf
+++ Infrastructure/Modules/Services/CloudWatch/main.tf
@@ -1,3 +1,27 @@
 resource "aws_cloudwatch_log_group" "main" {
   name = "cloudwatch-log-group"
 }
+
+resource "aws_cloudwatch_metric_alarm" "cpu_ecs_metric_alarm" {
+  alarm_name          = "ecr_alarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 4
+  metric_name         = "CPUUtilization"
+  namespace           = "ECS/ContainerInsights"
+  period              = 120
+  statistic           = "Average"
+  threshold           = 80
+  alarm_description   = "This metric monitors ecs container cpu utilization"
+}
+
+resource "aws_cloudwatch_metric_alarm" "memory_ecs_metric_alarm" {
+  alarm_name          = "ecr_alarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 4
+  metric_name         = "CPUUtilization"
+  namespace           = "ECS/ContainerInsights"
+  period              = 120
+  statistic           = "Average"
+  threshold           = 80
+  alarm_description   = "This metric monitors ecs container cpu utilization"
+}
